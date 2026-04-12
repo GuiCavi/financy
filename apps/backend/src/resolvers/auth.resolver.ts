a85b890a613497920214243d8085d78b56b6f427
Guilherme Cavichioli <cavichioli.guilherme@gmail.com>
2026-04-12 13:03:20 -0300

    feat(resolver): Wire up login resolver

--- apps/backend/src/resolvers/auth.resolver.ts
+++ apps/backend/src/resolvers/auth.resolver.ts
@@ -1,7 +1,7 @@
 import { Arg, Mutation, Resolver } from "type-graphql";
 
-import { RegisterInput } from "../dtos/input/auth.input";
-import { RegisterOutput } from "../dtos/output/auth.output";
+import { LoginInput, RegisterInput } from "../dtos/input/auth.input";
+import { LoginOutput, RegisterOutput } from "../dtos/output/auth.output";
 import { AuthService } from "../services/auth.service";
 
 @Resolver()
@@ -13,5 +13,12 @@ export class AuthResolver {
     @Arg("data", () => RegisterInput) data: RegisterInput,
   ): Promise<RegisterOutput> {
     return this.authService.register(data);
+  }
+
+  @Mutation(() => LoginOutput)
+  async login(
+    @Arg("data", () => LoginInput) data: LoginInput,
+  ): Promise<LoginOutput> {
+    return this.authService.login(data);
   }
 }
