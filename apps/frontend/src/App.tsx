a75f79da22dc6c940c1d930893985ce5a2e57690
Guilherme Cavichioli <cavichioli.guilherme@gmail.com>
2026-04-18 13:40:31 -0300

    feat(frontend): integrate theme provider and mode toggle in app

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { ModeToggle } from "@/components/toggle-theme";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">teste</h1>
+      <ModeToggle />
     </>
   );
 }
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import { ThemeProvider } from "./providers/theme-provider";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
